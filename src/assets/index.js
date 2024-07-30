43b6a039501779910d30dc530c4c0199cedbfb2e
igirb <139497578+igirb@users.noreply.github.com>
2024-07-30 18:42:55 +0200

    Feature: implement experience section | Refactor: delete non-relevant images and upload new ones

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,42 +21,42 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
+import bluesun from "./company/bluesun.png";
 import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import corvin from "./company/corvin.png";
+import unideb from "./company/unideb.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  logo,
-  fullstack,
-  devops,
-  cybersec,
-  uiux,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+    logo,
+    fullstack,
+    devops,
+    cybersec,
+    uiux,
+    github,
+    menu,
+    close,
+    css,
+    docker,
+    figma,
+    git,
+    html,
+    javascript,
+    mongodb,
+    nodejs,
+    reactjs,
+    redux,
+    tailwind,
+    typescript,
+    threejs,
+    bluesun,
+    starbucks,
+    corvin,
+    unideb,
+    carrent,
+    jobit,
+    tripguide,
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hello, I'm <span className='text-[#915eff]'>Brig</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            An enthusiast Full-Stack Developer,<br className='sm:block hidden'/> DevOps Engineer and future Cybersecurity Engineer
+            An enthusiastic Full-Stack Developer,<br className='sm:block hidden'/> DevOps Engineer and future Cybersecurity Engineer
           </p>
         </div>
       </div>
