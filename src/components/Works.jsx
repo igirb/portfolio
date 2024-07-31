35512951e74e119c4f004d652428f598ff12c380
igirb <139497578+igirb@users.noreply.github.com>
2024-07-31 17:13:24 +0200

    Feature: Works.jsx base code

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return (
-    <div>Works</div>
+      <>
+        <motion.div variants={textVariant()}
+                    initial="hidden"
+                    animate="show">
+          <p className={styles.sectionSubText}>My treasures</p>
+          <h2 className={styles.sectionHeadText}>Projects.</h2>
+        </motion.div>
+      </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, '');
