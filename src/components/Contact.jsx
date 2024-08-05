b91348ae0341f15ec353e2b0ccbcd54e64f78dd6
igirb <139497578+igirb@users.noreply.github.com>
2024-08-05 13:17:44 +0200

    Refactor: Contact component changed to social media buttons instead of emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,5 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -8,129 +7,55 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    emailjs
-        .send(
-            import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-            import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-            {
-              from_name: form.name,
-              to_name: "Brig Jasz",
-              from_email: form.email,
-              to_email: "brigijasz007@gmail.com",
-              message: form.message,
-            },
-            import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-        )
-        .then(
-            () => {
-              setLoading(false);
-              alert("Thank you! I will get back to you as soon as possible.");
-
-              setForm({
-                name: "",
-                email: "",
-                message: "",
-              });
-            },
-            (error) => {
-              setLoading(false);
-              console.error(error);
-
-              alert("Ahh, something went wrong. Please try again.");
-            }
-        );
-  };
-
-  return (
-      <div
-          className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-      >
-        <motion.div
-            variants={slideIn("left", "tween", 0.2, 1)}
-            className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
-        >
-          <p className={styles.sectionSubText}>Get in touch</p>
-          <h3 className={styles.sectionHeadText}>Contact.</h3>
-
-          <form
-              ref={formRef}
-              onSubmit={handleSubmit}
-              className='mt-10 flex flex-col gap-8'
-          >
-            <label className='flex flex-col'>
-              <span className='text-white font-medium mb-2'>Your Name</span>
-              <input
-                  type='text'
-                  name='name'
-                  value={form.name}
-                  onChange={handleChange}
-                  placeholder="What's your name?"
-                  className='bg-tertiary py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-              />
-            </label>
-            <label className='flex flex-col'>
-              <span className='text-white font-medium mb-2'>Your email</span>
-              <input
-                  type='email'
-                  name='email'
-                  value={form.email}
-                  onChange={handleChange}
-                  placeholder="What's your address?"
-                  className='bg-tertiary py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-              />
-            </label>
-            <label className='flex flex-col'>
-              <span className='text-white font-medium mb-2'>Your Message</span>
-              <textarea
-                  rows={4}
-                  name='message'
-                  value={form.message}
-                  onChange={handleChange}
-                  placeholder='What would you like to say?'
-                  className='bg-tertiary py-2 px-4 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-              />
-            </label>
-
-            <button
-                type='submit'
-                className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+    return (
+        <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
+            <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
             >
-              {loading ? "Sending..." : "Send"}
-            </button>
-          </form>
-        </motion.div>
+                <p className={styles.sectionSubText}>Get in touch</p>
+                <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <motion.div
-            variants={slideIn("right", "tween", 0.2, 1)}
-            className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-        >
-          <EarthCanvas />
-        </motion.div>
-      </div>
-  );
+                <div className="mt-10 flex flex-col gap-4">
+                    <div className="flex flex-col">
+                        <span className="text-white font-medium mb-4">Join forces with me and create something extraordinary together! 🌟</span>
+                        <a
+                            href="https://www.linkedin.com/in/igirb"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bg-tertiary py-3 px-6 rounded-lg text-white font-medium text-center shadow-md shadow-primary hover:bg-primary transition-colors mt-4"
+                        >
+                            LinkedIn
+                        </a>
+                        <span className="text-white font-medium mt-8">Or catch me on: </span>
+                        <a
+                            href="https://www.facebook.com/brigijasz"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bg-tertiary py-3 px-6 rounded-lg text-white font-medium text-center shadow-md shadow-primary hover:bg-primary transition-colors mt-4"
+                        >
+                            Facebook
+                        </a>
+                        <a
+                            href="https://www.instagram.com/iigirb/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bg-tertiary py-3 px-6 rounded-lg text-white font-medium text-center shadow-md shadow-primary hover:bg-primary transition-colors mt-4"
+                        >
+                            Instagram
+                        </a>
+                    </div>
+                </div>
+            </motion.div>
+
+            <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+            >
+                <EarthCanvas/>
+            </motion.div>
+        </div>
+    );
 };
 
 export default SectionWrapper(Contact, "contact");
